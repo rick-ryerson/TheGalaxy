00efcc540700793a90242f25e7068a20466f807c
BATMAN\rickr <rjryerson@gmail.com>
2021-11-10 11:18:23 -0500

    added service

---
+++ Source/GalacticSenate.Library/MaritalStatusType/MaritalStatusTypeService.cs
@@ -0,0 +1,207 @@
+﻿using GalacticSenate.Data.Implementations.EntityFramework;
+using GalacticSenate.Data.Interfaces;
+using GalacticSenate.Domain.Exceptions;
+using GalacticSenate.Library.MaritalStatusType.Requests;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Model = GalacticSenate.Domain.Model;
+
+namespace GalacticSenate.Library.MaritalStatusType
+{
+    public interface IMaritalStatusTypeService
+    {
+        Task<ModelResponse<Model.MaritalStatusType, AddMaritalStatusTypeRequest>> AddAsync(AddMaritalStatusTypeRequest request);
+        Task<BasicResponse<DeleteMaritalStatusTypeRequest>> DeleteAsync(DeleteMaritalStatusTypeRequest request);
+        Task<ModelResponse<Model.MaritalStatusType, ReadMaritalStatusTypeMultiRequest>> ReadAsync(ReadMaritalStatusTypeMultiRequest request);
+        Task<ModelResponse<Model.MaritalStatusType, ReadMaritalStatusTypeRequest>> ReadAsync(ReadMaritalStatusTypeRequest request);
+        Task<ModelResponse<Model.MaritalStatusType, ReadMaritalStatusTypeValueRequest>> ReadAsync(ReadMaritalStatusTypeValueRequest request);
+        Task<ModelResponse<Model.MaritalStatusType, UpdateMaritalStatusTypeRequest>> UpdateAsync(UpdateMaritalStatusTypeRequest request);
+    }
+
+    public class MaritalStatusTypeService : IMaritalStatusTypeService
+    {
+        private readonly IUnitOfWork<DataContext> unitOfWork;
+        private readonly IMaritalStatusTypeRepository genderRepository;
+
+        public MaritalStatusTypeService(IUnitOfWork<DataContext> unitOfWork)
+        {
+            this.unitOfWork = unitOfWork ?? throw new ArgumentNullException(nameof(unitOfWork));
+            this.genderRepository = unitOfWork.GetMaritalStatusTypeRepository() ?? throw new ApplicationException("Couldn't create gender repository");
+        }
+
+        public async Task<ModelResponse<Model.MaritalStatusType, AddMaritalStatusTypeRequest>> AddAsync(AddMaritalStatusTypeRequest request)
+        {
+            var response = new ModelResponse<Model.MaritalStatusType, AddMaritalStatusTypeRequest>(DateTime.Now, request);
+
+            var existing = await genderRepository.GetExactAsync(request.Value);
+
+            try
+            {
+                if (request is null)
+                    throw new ArgumentNullException(nameof(request));
+                if (string.IsNullOrEmpty(request.Value))
+                    throw new ArgumentNullException(nameof(request.Value));
+
+                if (existing is null)
+                {
+                    existing = await genderRepository.AddAsync(new Model.MaritalStatusType { Value = request.Value });
+                    unitOfWork.Save();
+
+                    response.Messages.Add($"MaritalStatusType with value {request.Value} added.");
+                }
+                else
+                {
+                    response.Messages.Add($"MaritalStatusType with value {request.Value} already exists.");
+                }
+
+                response.Results.Add(existing);
+
+                response.Status = StatusEnum.Successful;
+            }
+            catch (Exception ex)
+            {
+                response.Status = StatusEnum.Failed;
+                response.Messages.Add(ex.Message);
+            }
+
+            return response.Finalize();
+        }
+        public async Task<ModelResponse<Model.MaritalStatusType, UpdateMaritalStatusTypeRequest>> UpdateAsync(UpdateMaritalStatusTypeRequest request)
+        {
+            if (request is null)
+                throw new ArgumentNullException(nameof(request));
+            if (string.IsNullOrEmpty(request.NewValue))
+                throw new ArgumentNullException(nameof(request.NewValue));
+
+            var response = new ModelResponse<Model.MaritalStatusType, UpdateMaritalStatusTypeRequest>(DateTime.Now, request);
+
+            Model.MaritalStatusType existing = null;
+
+            try
+            {
+                existing = await genderRepository.GetAsync(request.Id);
+            }
+            catch (Exception ex)
+            {
+                response.Messages.Add(ex.Message);
+                response.Status = StatusEnum.Failed;
+            }
+
+            if (existing is null)
+            {
+                response.Messages.Add($"MaritalStatusType with id {request.Id} does not exist.");
+                response.Status = StatusEnum.Failed;
+            }
+            else
+            {
+                try
+                {
+                    var oldValue = existing.Value;
+
+                    if (oldValue == request.NewValue)
+                    {
+                        response.Messages.Add($"MaritalStatusType with id {existing.Id} already has a value of {oldValue}.");
+                    }
+                    else
+                    {
+                        existing.Value = request.NewValue;
+
+                        genderRepository.Update(existing);
+                        unitOfWork.Save();
+
+                        response.Messages.Add($"MaritalStatusType with id {existing.Id} updated from {oldValue} to {existing.Value}.");
+                    }
+
+                    response.Results.Add(existing);
+
+                    response.Status = StatusEnum.Successful;
+                }
+                catch (Exception ex)
+                {
+                    response.Messages.Add(ex.Message);
+                    response.Status = StatusEnum.Failed;
+                }
+            }
+
+            return response.Finalize();
+        }
+
+        public async Task<ModelResponse<Model.MaritalStatusType, ReadMaritalStatusTypeMultiRequest>> ReadAsync(ReadMaritalStatusTypeMultiRequest request)
+        {
+            var response = new ModelResponse<Model.MaritalStatusType, ReadMaritalStatusTypeMultiRequest>(DateTime.Now, request);
+
+            try
+            {
+                response.Results.AddRange(genderRepository.Get(request.PageIndex, request.PageSize));
+
+                response.Status = StatusEnum.Successful;
+            }
+            catch (Exception ex)
+            {
+                response.Messages.Add(ex.Message);
+                response.Status = StatusEnum.Failed;
+            }
+
+            return await Task.Run(() => { return response.Finalize(); });
+        }
+        public async Task<ModelResponse<Model.MaritalStatusType, ReadMaritalStatusTypeValueRequest>> ReadAsync(ReadMaritalStatusTypeValueRequest request)
+        {
+            var response = new ModelResponse<Model.MaritalStatusType, ReadMaritalStatusTypeValueRequest>(DateTime.Now, request);
+
+            try
+            {
+                if (request.Exact)
+                    response.Results.Add(await genderRepository.GetExactAsync(request.Value));
+                else
+                    response.Results.AddRange(genderRepository.GetContains(request.Value));
+
+                response.Status = StatusEnum.Successful;
+            }
+            catch (Exception ex)
+            {
+                response.Messages.Add(ex.Message);
+                response.Status = StatusEnum.Failed;
+            }
+            return response.Finalize();
+        }
+        public async Task<ModelResponse<Model.MaritalStatusType, ReadMaritalStatusTypeRequest>> ReadAsync(ReadMaritalStatusTypeRequest request)
+        {
+            var response = new ModelResponse<Model.MaritalStatusType, ReadMaritalStatusTypeRequest>(DateTime.Now, request);
+
+            try
+            {
+                response.Results.Add(await genderRepository.GetAsync(request.Id));
+
+                response.Status = StatusEnum.Successful;
+            }
+            catch (Exception ex)
+            {
+                response.Messages.Add(ex.Message);
+                response.Status = StatusEnum.Failed;
+            }
+            return response.Finalize();
+        }
+
+        public async Task<BasicResponse<DeleteMaritalStatusTypeRequest>> DeleteAsync(DeleteMaritalStatusTypeRequest request)
+        {
+            var response = new BasicResponse<DeleteMaritalStatusTypeRequest>(DateTime.Now, request);
+
+            try
+            {
+                await genderRepository.DeleteAsync(request.Id);
+                unitOfWork.Save();
+
+                response.Status = StatusEnum.Successful;
+            }
+            catch (Exception ex)
+            {
+                response.Status = StatusEnum.Failed;
+                response.Messages.Add(ex.Message);
+            }
+
+            return response.Finalize();
+        }
+    }
+}
