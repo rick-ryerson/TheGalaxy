57f5b3fe706ac5988b64b539d979672e43b3b2c8
BATMAN\rickr <rjryerson@gmail.com>
2021-11-10 10:59:20 -0500

    added requests

---
+++ Source/GalacticSenate.Library/MaritalStatusType/Requests/DeleteMaritalStatusTypeRequest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GalacticSenate.Library.Gender.Requests
+{
+    public class DeleteMaritalStatusTypeRequest
+    {
+        public int Id { get; set; }
+    }
+}
