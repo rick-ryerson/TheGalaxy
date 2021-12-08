a62bc7a3c1c5b0c6743f4c8d2bbe32e6205d52bb
BATMAN\rickr <rjryerson@gmail.com>
2021-12-07 19:34:02 -0500

    added unit tests

---
+++ Tests/GalacticSenate.Tests/MaritalStatusTypeServiceUnitTests.cs
@@ -0,0 +1,121 @@
+using GalacticSenate.Data.Implementations.EntityFramework;
+using GalacticSenate.Data.Interfaces;
+using GalacticSenate.Library;
+using GalacticSenate.Library.MaritalStatusType;
+using GalacticSenate.Library.MaritalStatusType.Requests;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GalacticSenate.Tests
+{
+    [TestClass]
+    public class MaritalStatusTypeServiceUnitTests : DatabaseFixture
+    {
+        private readonly IMaritalStatusTypeService MaritalStatusTypeService;
+        private readonly UnitOfWork unitOfWork;
+
+        public MaritalStatusTypeServiceUnitTests() : base("DataContext")
+        {
+            unitOfWork = new UnitOfWork(dataContext);
+            var maritalStatusTypeRepository = unitOfWork.GetMaritalStatusTypeRepository();
+            MaritalStatusTypeService = new MaritalStatusTypeService(unitOfWork);
+        }
+        [TestMethod]
+        public async Task Add_Test()
+        {
+            var addResponse = await MaritalStatusTypeService.AddAsync(new AddMaritalStatusTypeRequest
+            {
+                Value = "Fourth"
+            });
+
+            Assert.IsTrue(addResponse.Status == StatusEnum.Successful);
+        }
+        [TestMethod]
+        public async Task AddMissingValue_Test()
+        {
+            var request = new AddMaritalStatusTypeRequest
+            {
+                Value = ""
+            };
+
+            var response = await MaritalStatusTypeService.AddAsync(request);
+
+            Assert.IsTrue(response.Status == StatusEnum.Failed);
+            Assert.IsTrue(response.Messages.Contains("Value cannot be null. (Parameter 'Value')"));
+        }
+        [TestMethod]
+        public async Task AddDuplicate_Test()
+        {
+            var request = new AddMaritalStatusTypeRequest
+            {
+                Value = "Duplicate"
+            };
+
+            var addResponse1 = await MaritalStatusTypeService.AddAsync(request);
+
+            var addResponse2 = await MaritalStatusTypeService.AddAsync(request);
+
+            Assert.IsTrue(addResponse1.Status == StatusEnum.Successful);
+            Assert.IsTrue(addResponse2.Messages.Contains($"MaritalStatusType with value {request.Value} already exists."));
+        }
+        [TestMethod]
+        public async Task ReadPaged_Test()
+        {
+            var readResponse = await MaritalStatusTypeService.ReadAsync(new ReadMaritalStatusTypeMultiRequest
+            {
+                PageIndex = 0,
+                PageSize = 2
+            });
+
+            Assert.IsTrue(readResponse.Status == StatusEnum.Successful);
+            Assert.IsTrue(readResponse.Results.Count == 2);
+        }
+        [TestMethod]
+        public async Task ReadAll_Test()
+        {
+            var readResponse = await MaritalStatusTypeService.ReadAsync(new ReadMaritalStatusTypeMultiRequest
+            {
+                PageIndex = 0,
+                PageSize = int.MaxValue
+            });
+
+            Assert.IsTrue(readResponse.Status == StatusEnum.Successful);
+        }
+        [TestMethod]
+        public async Task UpdateExisting_Test()
+        {
+            var getResponse = await MaritalStatusTypeService.ReadAsync(new ReadMaritalStatusTypeMultiRequest
+            {
+                PageIndex = 0,
+                PageSize = 1
+            });
+
+            var request = new UpdateMaritalStatusTypeRequest
+            {
+                Id = getResponse.Results.First().Id,
+                NewValue = string.Concat("updated-", getResponse.Results.First().Value)
+            };
+
+            var updateResponse = await MaritalStatusTypeService.UpdateAsync(request);
+
+            Assert.IsTrue(updateResponse.Status == StatusEnum.Successful);
+            Assert.IsTrue(updateResponse.Results.FirstOrDefault().Value == request.NewValue);
+        }
+        [TestMethod]
+        public async Task UpdateNonExisting_Test()
+        {
+            var request = new UpdateMaritalStatusTypeRequest
+            {
+                Id = -1,
+                NewValue = "test"
+            };
+
+            var updateResponse = await MaritalStatusTypeService.UpdateAsync(request);
+
+            Assert.IsTrue(updateResponse.Status == StatusEnum.Failed);
+            Assert.IsTrue(updateResponse.Messages.Contains($"MaritalStatusType with id {request.Id} does not exist."));
+        }
+    }
+}
---
+++ Tests/GalacticSenate.Tests/PersonNameTypeServiceUnitTests.cs
@@ -0,0 +1,121 @@
+using GalacticSenate.Data.Implementations.EntityFramework;
+using GalacticSenate.Data.Interfaces;
+using GalacticSenate.Library;
+using GalacticSenate.Library.PersonNameType;
+using GalacticSenate.Library.PersonNameType.Requests;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GalacticSenate.Tests
+{
+    [TestClass]
+    public class PersonNameTypeServiceUnitTests : DatabaseFixture
+    {
+        private readonly IPersonNameTypeService personNameTypeService;
+        private readonly UnitOfWork unitOfWork;
+
+        public PersonNameTypeServiceUnitTests() : base("DataContext")
+        {
+            unitOfWork = new UnitOfWork(dataContext);
+            var personNameTypeRepository = unitOfWork.GetPersonNameTypeRepository();
+            personNameTypeService = new PersonNameTypeService(unitOfWork);
+        }
+        [TestMethod]
+        public async Task Add_Test()
+        {
+            var addResponse = await personNameTypeService.AddAsync(new AddPersonNameTypeRequest
+            {
+                Value = "Fourth"
+            });
+
+            Assert.IsTrue(addResponse.Status == StatusEnum.Successful);
+        }
+        [TestMethod]
+        public async Task AddMissingValue_Test()
+        {
+            var request = new AddPersonNameTypeRequest
+            {
+                Value = ""
+            };
+
+            var response = await personNameTypeService.AddAsync(request);
+
+            Assert.IsTrue(response.Status == StatusEnum.Failed);
+            Assert.IsTrue(response.Messages.Contains("Value cannot be null. (Parameter 'Value')"));
+        }
+        [TestMethod]
+        public async Task AddDuplicate_Test()
+        {
+            var request = new AddPersonNameTypeRequest
+            {
+                Value = "Duplicate"
+            };
+
+            var addResponse1 = await personNameTypeService.AddAsync(request);
+
+            var addResponse2 = await personNameTypeService.AddAsync(request);
+
+            Assert.IsTrue(addResponse1.Status == StatusEnum.Successful);
+            Assert.IsTrue(addResponse2.Messages.Contains($"PersonNameType with value {request.Value} already exists."));
+        }
+        [TestMethod]
+        public async Task ReadPaged_Test()
+        {
+            var readResponse = await personNameTypeService.ReadAsync(new ReadPersonNameTypeMultiRequest
+            {
+                PageIndex = 0,
+                PageSize = 2
+            });
+
+            Assert.IsTrue(readResponse.Status == StatusEnum.Successful);
+            Assert.IsTrue(readResponse.Results.Count == 2);
+        }
+        [TestMethod]
+        public async Task ReadAll_Test()
+        {
+            var readResponse = await personNameTypeService.ReadAsync(new ReadPersonNameTypeMultiRequest
+            {
+                PageIndex = 0,
+                PageSize = int.MaxValue
+            });
+
+            Assert.IsTrue(readResponse.Status == StatusEnum.Successful);
+        }
+        [TestMethod]
+        public async Task UpdateExisting_Test()
+        {
+            var getResponse = await personNameTypeService.ReadAsync(new ReadPersonNameTypeMultiRequest
+            {
+                PageIndex = 0,
+                PageSize = 1
+            });
+
+            var request = new UpdatePersonNameTypeRequest
+            {
+                Id = getResponse.Results.First().Id,
+                NewValue = string.Concat("updated-", getResponse.Results.First().Value)
+            };
+
+            var updateResponse = await personNameTypeService.UpdateAsync(request);
+
+            Assert.IsTrue(updateResponse.Status == StatusEnum.Successful);
+            Assert.IsTrue(updateResponse.Results.FirstOrDefault().Value == request.NewValue);
+        }
+        [TestMethod]
+        public async Task UpdateNonExisting_Test()
+        {
+            var request = new UpdatePersonNameTypeRequest
+            {
+                Id = -1,
+                NewValue = "test"
+            };
+
+            var updateResponse = await personNameTypeService.UpdateAsync(request);
+
+            Assert.IsTrue(updateResponse.Status == StatusEnum.Failed);
+            Assert.IsTrue(updateResponse.Messages.Contains($"PersonNameType with id {request.Id} does not exist."));
+        }
+    }
+}
