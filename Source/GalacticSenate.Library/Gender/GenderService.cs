6d2e3beba52b643ce632949d3e60f99859e4c696
Rick Ryerson <rick.ryerson@outlook.com>
2021-12-10 22:12:44 -0500

    tighten gender service tests

--- Source/GalacticSenate.Library/Gender/GenderService.cs
+++ Source/GalacticSenate.Library/Gender/GenderService.cs
@@ -8,203 +8,169 @@ using System.Text;
 using System.Threading.Tasks;
 using Model = GalacticSenate.Domain.Model;
 
-namespace GalacticSenate.Library.Gender
-{
-    public interface IGenderService
-    {
-        Task<ModelResponse<Model.Gender, AddGenderRequest>> AddAsync(AddGenderRequest request);
-        Task<BasicResponse<DeleteGenderRequest>> DeleteAsync(DeleteGenderRequest request);
-        Task<ModelResponse<Model.Gender, ReadGenderMultiRequest>> ReadAsync(ReadGenderMultiRequest request);
-        Task<ModelResponse<Model.Gender, ReadGenderRequest>> ReadAsync(ReadGenderRequest request);
-        Task<ModelResponse<Model.Gender, ReadGenderValueRequest>> ReadAsync(ReadGenderValueRequest request);
-        Task<ModelResponse<Model.Gender, UpdateGenderRequest>> UpdateAsync(UpdateGenderRequest request);
-    }
+namespace GalacticSenate.Library.Gender {
+   public interface IGenderService {
+      Task<ModelResponse<Model.Gender, AddGenderRequest>> AddAsync(AddGenderRequest request);
+      Task<BasicResponse<DeleteGenderRequest>> DeleteAsync(DeleteGenderRequest request);
+      Task<ModelResponse<Model.Gender, ReadGenderMultiRequest>> ReadAsync(ReadGenderMultiRequest request);
+      Task<ModelResponse<Model.Gender, ReadGenderRequest>> ReadAsync(ReadGenderRequest request);
+      Task<ModelResponse<Model.Gender, ReadGenderValueRequest>> ReadAsync(ReadGenderValueRequest request);
+      Task<ModelResponse<Model.Gender, UpdateGenderRequest>> UpdateAsync(UpdateGenderRequest request);
+   }
 
-    public class GenderService : IGenderService
-    {
-        private readonly IUnitOfWork<DataContext> unitOfWork;
-        private readonly IGenderRepository genderRepository;
+   public class GenderService : IGenderService {
+      private readonly IUnitOfWork<DataContext> unitOfWork;
+      private readonly IGenderRepository genderRepository;
 
-        public GenderService(IUnitOfWork<DataContext> unitOfWork)
-        {
-            this.unitOfWork = unitOfWork ?? throw new ArgumentNullException(nameof(unitOfWork));
-            this.genderRepository = unitOfWork.GetGenderRepository() ?? throw new ApplicationException("Couldn't create gender repository");
-        }
+      public GenderService(IUnitOfWork<DataContext> unitOfWork) {
+         this.unitOfWork = unitOfWork ?? throw new ArgumentNullException(nameof(unitOfWork));
+         this.genderRepository = unitOfWork.GetGenderRepository() ?? throw new ApplicationException("Couldn't create gender repository");
+      }
 
-        public async Task<ModelResponse<Model.Gender, AddGenderRequest>> AddAsync(AddGenderRequest request)
-        {
-            var response = new ModelResponse<Model.Gender, AddGenderRequest>(DateTime.Now, request);
+      public async Task<ModelResponse<Model.Gender, AddGenderRequest>> AddAsync(AddGenderRequest request) {
+         var response = new ModelResponse<Model.Gender, AddGenderRequest>(DateTime.Now, request);
 
-            var existing = await genderRepository.GetExactAsync(request.Value);
+         var existing = await genderRepository.GetExactAsync(request.Value);
 
-            try
-            {
-                if (request is null)
-                    throw new ArgumentNullException(nameof(request));
-                if (string.IsNullOrEmpty(request.Value))
-                    throw new ArgumentNullException(nameof(request.Value));
-
-                if (existing is null)
-                {
-                    existing = await genderRepository.AddAsync(new Model.Gender { Value = request.Value });
-                    unitOfWork.Save();
-
-                    response.Messages.Add($"Gender with value {request.Value} added.");
-                }
-                else
-                {
-                    response.Messages.Add($"Gender with value {request.Value} already exists.");
-                }
-
-                response.Results.Add(existing);
-
-                response.Status = StatusEnum.Successful;
-            }
-            catch (Exception ex)
-            {
-                response.Status = StatusEnum.Failed;
-                response.Messages.Add(ex.Message);
-            }
-
-            return response.Finalize();
-        }
-        public async Task<ModelResponse<Model.Gender, UpdateGenderRequest>> UpdateAsync(UpdateGenderRequest request)
-        {
+         try {
             if (request is null)
-                throw new ArgumentNullException(nameof(request));
-            if (string.IsNullOrEmpty(request.NewValue))
-                throw new ArgumentNullException(nameof(request.NewValue));
+               throw new ArgumentNullException(nameof(request));
+            if (string.IsNullOrEmpty(request.Value))
+               throw new ArgumentNullException(nameof(request.Value));
 
-            var response = new ModelResponse<Model.Gender, UpdateGenderRequest>(DateTime.Now, request);
+            if (existing is null) {
+               existing = await genderRepository.AddAsync(new Model.Gender { Value = request.Value });
+               unitOfWork.Save();
 
-            Model.Gender existing = null;
-
-            try
-            {
-                existing = await genderRepository.GetAsync(request.Id);
-            }
-            catch (Exception ex)
-            {
-                response.Messages.Add(ex.Message);
-                response.Status = StatusEnum.Failed;
+               response.Messages.Add($"Gender with value {request.Value} added.");
+            } else {
+               response.Messages.Add($"Gender with value {request.Value} already exists.");
             }
 
-            if (existing is null)
-            {
-                response.Messages.Add($"Gender with id {request.Id} does not exist.");
-                response.Status = StatusEnum.Failed;
+            response.Results.Add(existing);
+
+            response.Status = StatusEnum.Successful;
+         } catch (Exception ex) {
+            response.Status = StatusEnum.Failed;
+            response.Messages.Add(ex.Message);
+         }
+
+         return response.Finalize();
+      }
+      public async Task<ModelResponse<Model.Gender, UpdateGenderRequest>> UpdateAsync(UpdateGenderRequest request) {
+         if (request is null)
+            throw new ArgumentNullException(nameof(request));
+         if (string.IsNullOrEmpty(request.NewValue))
+            throw new ArgumentNullException(nameof(request.NewValue));
+
+         var response = new ModelResponse<Model.Gender, UpdateGenderRequest>(DateTime.Now, request);
+
+         Model.Gender existing = null;
+
+         try {
+            existing = await genderRepository.GetAsync(request.Id);
+         } catch (Exception ex) {
+            response.Messages.Add(ex.Message);
+            response.Status = StatusEnum.Failed;
+         }
+
+         if (existing is null) {
+            response.Messages.Add($"Gender with id {request.Id} does not exist.");
+            response.Status = StatusEnum.Failed;
+         } else {
+            try {
+               var oldValue = existing.Value;
+
+               if (oldValue == request.NewValue) {
+                  response.Messages.Add($"Gender with id {existing.Id} already has a value of {oldValue}.");
+               } else {
+                  existing.Value = request.NewValue;
+
+                  genderRepository.Update(existing);
+                  unitOfWork.Save();
+
+                  response.Messages.Add($"Gender with id {existing.Id} updated from {oldValue} to {existing.Value}.");
+               }
+
+               response.Results.Add(existing);
+
+               response.Status = StatusEnum.Successful;
+            } catch (Exception ex) {
+               response.Messages.Add(ex.Message);
+               response.Status = StatusEnum.Failed;
             }
-            else
-            {
-                try
-                {
-                    var oldValue = existing.Value;
+         }
 
-                    if (oldValue == request.NewValue)
-                    {
-                        response.Messages.Add($"Gender with id {existing.Id} already has a value of {oldValue}.");
-                    }
-                    else
-                    {
-                        existing.Value = request.NewValue;
+         return response.Finalize();
+      }
 
-                        genderRepository.Update(existing);
-                        unitOfWork.Save();
+      public async Task<ModelResponse<Model.Gender, ReadGenderMultiRequest>> ReadAsync(ReadGenderMultiRequest request) {
+         var response = new ModelResponse<Model.Gender, ReadGenderMultiRequest>(DateTime.Now, request);
 
-                        response.Messages.Add($"Gender with id {existing.Id} updated from {oldValue} to {existing.Value}.");
-                    }
+         try {
+            response.Results.AddRange(genderRepository.Get(request.PageIndex, request.PageSize));
 
-                    response.Results.Add(existing);
+            response.Status = StatusEnum.Successful;
+         } catch (Exception ex) {
+            response.Messages.Add(ex.Message);
+            response.Status = StatusEnum.Failed;
+         }
 
-                    response.Status = StatusEnum.Successful;
-                }
-                catch (Exception ex)
-                {
-                    response.Messages.Add(ex.Message);
-                    response.Status = StatusEnum.Failed;
-                }
-            }
+         return await Task.Run(() => { return response.Finalize(); });
+      }
+      public async Task<ModelResponse<Model.Gender, ReadGenderValueRequest>> ReadAsync(ReadGenderValueRequest request) {
+         var response = new ModelResponse<Model.Gender, ReadGenderValueRequest>(DateTime.Now, request);
 
-            return response.Finalize();
-        }
+         try {
+            if (request.Exact) {
+               var r = await genderRepository.GetExactAsync(request.Value);
 
-        public async Task<ModelResponse<Model.Gender, ReadGenderMultiRequest>> ReadAsync(ReadGenderMultiRequest request)
-        {
-            var response = new ModelResponse<Model.Gender, ReadGenderMultiRequest>(DateTime.Now, request);
+               if (r != null) {
+                  response.Results.Add(r);
+               }
 
-            try
-            {
-                response.Results.AddRange(genderRepository.Get(request.PageIndex, request.PageSize));
 
-                response.Status = StatusEnum.Successful;
-            }
-            catch (Exception ex)
-            {
-                response.Messages.Add(ex.Message);
-                response.Status = StatusEnum.Failed;
-            }
+            } else
+               response.Results.AddRange(genderRepository.GetContains(request.Value));
 
-            return await Task.Run(() => { return response.Finalize(); });
-        }
-        public async Task<ModelResponse<Model.Gender, ReadGenderValueRequest>> ReadAsync(ReadGenderValueRequest request)
-        {
-            var response = new ModelResponse<Model.Gender, ReadGenderValueRequest>(DateTime.Now, request);
+            response.Status = StatusEnum.Successful;
+         } catch (Exception ex) {
+            response.Messages.Add(ex.Message);
+            response.Status = StatusEnum.Failed;
+         }
+         return response.Finalize();
+      }
+      public async Task<ModelResponse<Model.Gender, ReadGenderRequest>> ReadAsync(ReadGenderRequest request) {
+         var response = new ModelResponse<Model.Gender, ReadGenderRequest>(DateTime.Now, request);
 
-            try
-            {
-                if (request.Exact)
-                    response.Results.Add(await genderRepository.GetExactAsync(request.Value));
-                else
-                    response.Results.AddRange(genderRepository.GetContains(request.Value));
+         try {
+            var gender = await genderRepository.GetAsync(request.Id);
 
-                response.Status = StatusEnum.Successful;
-            }
-            catch (Exception ex)
-            {
-                response.Messages.Add(ex.Message);
-                response.Status = StatusEnum.Failed;
-            }
-            return response.Finalize();
-        }
-        public async Task<ModelResponse<Model.Gender, ReadGenderRequest>> ReadAsync(ReadGenderRequest request)
-        {
-            var response = new ModelResponse<Model.Gender, ReadGenderRequest>(DateTime.Now, request);
+            if (gender != null)
+               response.Results.Add(await genderRepository.GetAsync(request.Id));
 
-            try
-            {
-                var gender = await genderRepository.GetAsync(request.Id);
+            response.Status = StatusEnum.Successful;
+         } catch (Exception ex) {
+            response.Messages.Add(ex.Message);
+            response.Status = StatusEnum.Failed;
+         }
+         return response.Finalize();
+      }
 
-                if (gender != null)
-                    response.Results.Add(await genderRepository.GetAsync(request.Id));
+      public async Task<BasicResponse<DeleteGenderRequest>> DeleteAsync(DeleteGenderRequest request) {
+         var response = new BasicResponse<DeleteGenderRequest>(DateTime.Now, request);
 
-                response.Status = StatusEnum.Successful;
-            }
-            catch (Exception ex)
-            {
-                response.Messages.Add(ex.Message);
-                response.Status = StatusEnum.Failed;
-            }
-            return response.Finalize();
-        }
+         try {
+            await genderRepository.DeleteAsync(request.Id);
+            unitOfWork.Save();
 
-        public async Task<BasicResponse<DeleteGenderRequest>> DeleteAsync(DeleteGenderRequest request)
-        {
-            var response = new BasicResponse<DeleteGenderRequest>(DateTime.Now, request);
+            response.Status = StatusEnum.Successful;
+         } catch (Exception ex) {
+            response.Status = StatusEnum.Failed;
+            response.Messages.Add(ex.Message);
+         }
 
-            try
-            {
-                await genderRepository.DeleteAsync(request.Id);
-                unitOfWork.Save();
-
-                response.Status = StatusEnum.Successful;
-            }
-            catch (Exception ex)
-            {
-                response.Status = StatusEnum.Failed;
-                response.Messages.Add(ex.Message);
-            }
-
-            return response.Finalize();
-        }
-    }
+         return response.Finalize();
+      }
+   }
 }
--- Tests/GalacticSenate.Tests/GenderServiceUnitTests.cs
+++ Tests/GalacticSenate.Tests/GenderServiceUnitTests.cs
@@ -8,114 +8,121 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace GalacticSenate.Tests
-{
-    [TestClass]
-    public class GenderServiceUnitTests : DatabaseFixture
-    {
-        private readonly IGenderService genderService;
-        private readonly UnitOfWork unitOfWork;
+namespace GalacticSenate.Tests {
+   [TestClass]
+   public class GenderServiceUnitTests : DatabaseFixture {
+      private readonly IGenderService genderService;
+      private readonly UnitOfWork unitOfWork;
 
-        public GenderServiceUnitTests() : base("DataContext")
-        {
-            unitOfWork = new UnitOfWork(dataContext);
-            var genderRepository = unitOfWork.GetGenderRepository();
-            genderService = new GenderService(unitOfWork);
-        }
-        [TestMethod]
-        public async Task Add_Test()
-        {
-            var addResponse = await genderService.AddAsync(new AddGenderRequest
-            {
-                Value = "Fourth"
-            });
+      public GenderServiceUnitTests() : base("DataContext") {
+         unitOfWork = new UnitOfWork(dataContext);
+         var genderRepository = unitOfWork.GetGenderRepository();
+         genderService = new GenderService(unitOfWork);
+      }
+      [TestMethod]
+      public async Task Add_Test() {
+         var addResponse = await genderService.AddAsync(new AddGenderRequest
+         {
+            Value = "Another"
+         });
 
-            Assert.IsTrue(addResponse.Status == StatusEnum.Successful);
-        }
-        [TestMethod]
-        public async Task AddMissingValue_Test()
-        {
-            var request = new AddGenderRequest
-            {
-                Value = ""
-            };
+         Assert.IsTrue(addResponse.Status == StatusEnum.Successful);
+      }
 
-            var response = await genderService.AddAsync(request);
+      [TestMethod]
+      public async Task AddMissingValue_Test() {
+         var request = new AddGenderRequest
+         {
+            Value = ""
+         };
 
-            Assert.IsTrue(response.Status == StatusEnum.Failed);
-            Assert.IsTrue(response.Messages.Contains("Value cannot be null. (Parameter 'Value')"));
-        }
-        [TestMethod]
-        public async Task AddDuplicate_Test()
-        {
-            var request = new AddGenderRequest
-            {
-                Value = "Duplicate"
-            };
+         var response = await genderService.AddAsync(request);
 
-            var addResponse1 = await genderService.AddAsync(request);
+         Assert.IsTrue(response.Status == StatusEnum.Failed);
+         Assert.IsTrue(response.Messages.Contains("Value cannot be null. (Parameter 'Value')"));
+      }
+      [TestMethod]
+      public async Task AddDuplicate_Test() {
+         var request = new AddGenderRequest
+         {
+            Value = "Male"
+         };
 
-            var addResponse2 = await genderService.AddAsync(request);
+         var response = await genderService.AddAsync(request);
 
-            Assert.IsTrue(addResponse1.Status == StatusEnum.Successful);
-            Assert.IsTrue(addResponse2.Messages.Contains($"Gender with value {request.Value} already exists."));
-        }
-        [TestMethod]
-        public async Task ReadPaged_Test()
-        {
-            var readResponse = await genderService.ReadAsync(new ReadGenderMultiRequest
-            {
-                PageIndex = 0,
-                PageSize = 2
-            });
+         Assert.IsTrue(response.Status == StatusEnum.Successful);
+         Assert.IsTrue(response.Messages.Contains($"Gender with value {request.Value} already exists."));
+      }
+      [TestMethod]
+      public async Task ReadPaged_Test() {
+         await genderService.AddAsync(new AddGenderRequest { Value = "Paged 1" });
+         await genderService.AddAsync(new AddGenderRequest { Value = "Paged 2" });
+         await genderService.AddAsync(new AddGenderRequest { Value = "Paged 3" });
+         await genderService.AddAsync(new AddGenderRequest { Value = "Paged 4" });
+         await genderService.AddAsync(new AddGenderRequest { Value = "Paged 5" });
 
-            Assert.IsTrue(readResponse.Status == StatusEnum.Successful);
-            Assert.IsTrue(readResponse.Results.Count == 2);
-        }
-        [TestMethod]
-        public async Task ReadAll_Test()
-        {
-            var readResponse = await genderService.ReadAsync(new ReadGenderMultiRequest
-            {
-                PageIndex = 0,
-                PageSize = int.MaxValue
-            });
+         var response = await genderService.ReadAsync(new ReadGenderMultiRequest
+         {
+            PageIndex = 0,
+            PageSize = 5
+         });
 
-            Assert.IsTrue(readResponse.Status == StatusEnum.Successful);
-        }
-        [TestMethod]
-        public async Task UpdateExisting_Test()
-        {
-            var getResponse = await genderService.ReadAsync(new ReadGenderMultiRequest
-            {
-                PageIndex = 0,
-                PageSize = 1
-            });
+         Assert.IsTrue(response.Status == StatusEnum.Successful);
+         Assert.IsTrue(response.Results.Count == 5);
+      }
+      [TestMethod]
+      public async Task ReadAll_Test() {
+         var readResponse = await genderService.ReadAsync(new ReadGenderMultiRequest
+         {
+            PageIndex = 0,
+            PageSize = int.MaxValue
+         });
 
-            var request = new UpdateGenderRequest
-            {
-                Id = getResponse.Results.First().Id,
-                NewValue = string.Concat("updated-", getResponse.Results.First().Value)
-            };
+         Assert.IsTrue(readResponse.Status == StatusEnum.Successful);
+         Assert.IsTrue(readResponse?.Results.Count > 1);
+      }
+      [TestMethod]
+      public async Task ReadExactExists_Test() {
+         var response = await genderService.ReadAsync(new ReadGenderValueRequest { Exact = true, Value = "Male" });
 
-            var updateResponse = await genderService.UpdateAsync(request);
+         Assert.IsTrue(response.Status == StatusEnum.Successful);
+         Assert.IsTrue(response.Results.Count == 1);
+      }
+      [TestMethod]
+      public async Task ReadExactNotExists_Test() {
 
-            Assert.IsTrue(updateResponse.Status == StatusEnum.Successful);
-            Assert.IsTrue(updateResponse.Results.FirstOrDefault().Value == request.NewValue);
-        }
-        [TestMethod]
-        public async Task UpdateNonExisting_Test()
-        {
-            var request = new UpdateGenderRequest
-            {
-                Id = -1,
-                NewValue = "test"
-            };
+         var response2 = await genderService.ReadAsync(new ReadGenderValueRequest { Exact = true, Value = "Mail" });
 
-            var updateResponse = await genderService.UpdateAsync(request);
+         Assert.IsTrue(response2.Status == StatusEnum.Successful);
+         Assert.IsTrue(response2.Results.Count == 0);
+      }
+      [TestMethod]
+      public async Task UpdateExisting_Test() {
+         var addResponse = await genderService.AddAsync(new AddGenderRequest { Value = "ToUpdate" });
 
-            Assert.IsTrue(updateResponse.Status == StatusEnum.Failed);
-            Assert.IsTrue(updateResponse.Messages.Contains($"Gender with id {request.Id} does not exist."));
-        }
-    }
+         var request = new UpdateGenderRequest
+         {
+            Id = addResponse.Results.First().Id,
+            NewValue = string.Concat("updated-", addResponse.Results.First().Value)
+         };
+
+         var updateResponse = await genderService.UpdateAsync(request);
+
+         Assert.IsTrue(updateResponse.Status == StatusEnum.Successful);
+         Assert.IsTrue(updateResponse.Results.FirstOrDefault().Value == request.NewValue);
+      }
+      [TestMethod]
+      public async Task UpdateNonExisting_Test() {
+         var request = new UpdateGenderRequest
+         {
+            Id = -1,
+            NewValue = "test"
+         };
+
+         var updateResponse = await genderService.UpdateAsync(request);
+
+         Assert.IsTrue(updateResponse.Status == StatusEnum.Failed);
+         Assert.IsTrue(updateResponse.Messages.Contains($"Gender with id {request.Id} does not exist."));
+      }
+   }
 }
