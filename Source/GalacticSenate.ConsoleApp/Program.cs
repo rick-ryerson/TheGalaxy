7a8ccbd2375fdf810585f7b5f2a9c500a8863ceb
Rick Ryerson <rick.ryerson@outlook.com>
2023-02-28 00:09:25 -0500

    celestial

--- Source/GalacticSenate.ConsoleApp/Program.cs
+++ Source/GalacticSenate.ConsoleApp/Program.cs
@@ -3,6 +3,9 @@ using GalacticSenate.Data.Implementations.EntityFramework;
 using System;
 using Microsoft.Extensions.Hosting;
 using GalacticSenate.Data.Seeding;
+using Microsoft.Extensions.DependencyInjection;
+using GalacticSenate.Data.Interfaces;
+using Microsoft.Extensions.Configuration;
 
 namespace GalacticSenate.ConsoleApp {
    class Program {
@@ -13,7 +16,21 @@ namespace GalacticSenate.ConsoleApp {
             .CreateDefaultBuilder(args)
             .ConfigureServices(services =>
             {
+               IConfiguration Configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddEnvironmentVariables()
+                .AddCommandLine(args)
+                .Build();
 
+               var optionsBuilder = new DbContextOptionsBuilder<DataContext>();
+
+               services.AddDbContext<DataContext>(options =>
+               {
+
+                  options.UseSqlServer(Configuration.GetConnectionString("DataContext"));
+               });
+
+               services.AddScoped<IUnitOfWork<DataContext>, UnitOfWork>();
             })
             .Build()
             .SeedData()
--- Source/GalacticSenate.WebApi/Program.cs
+++ Source/GalacticSenate.WebApi/Program.cs
@@ -7,22 +7,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace GalacticSenate.WebApi
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World from WebApi!");
+namespace GalacticSenate.WebApi {
+   public class Program {
+      public static void Main(string[] args) {
+         Console.WriteLine("Hello World from WebApi!");
 
-            CreateHostBuilder(args).Build().Run();
-        }
+         CreateHostBuilder(args).Build().Run();
+      }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+      public static IHostBuilder CreateHostBuilder(string[] args) =>
+          Host.CreateDefaultBuilder(args)
+              .ConfigureWebHostDefaults(webBuilder =>
+              {
+                 webBuilder.UseStartup<Startup>();
+              });
+   }
 }
