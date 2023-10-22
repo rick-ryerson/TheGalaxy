6a12da44cc8aa453e7c1969910ef3b1b44f84e2c
Rick Ryerson <rick.ryerson@outlook.com>
2023-10-22 09:36:53 -0400

    identityserver4 working

---
+++ Source/JediOrder.Web/Config.cs
@@ -0,0 +1,45 @@
+﻿using IdentityServer4;
+using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace JediOrder.Web {
+   public static class Config {
+      public static IEnumerable<IdentityResource> IdentityResources =>
+         new List<IdentityResource>
+         {
+            new IdentityResources.OpenId(),
+            new IdentityResources.Profile()
+         };
+
+      public static IEnumerable<ApiScope> ApiScopes =>
+         new List<ApiScope>
+         {
+            new ApiScope("api1", "My API")
+         };
+      public static IEnumerable<Client> Clients =>
+         new List<Client>
+         {
+            new Client
+            {
+               ClientId = "client",
+               ClientSecrets = { new Secret("secret".Sha256()) },
+               AllowedGrantTypes = GrantTypes.ClientCredentials,
+               AllowedScopes = { "api1" }
+            },
+            new Client
+            {
+               ClientId = "mvc",
+               ClientSecrets = { new Secret("secret".Sha256()) },
+               AllowedGrantTypes = GrantTypes.Code,
+               RedirectUris = { "https://localhost:5002/signin-oidc" },
+               PostLogoutRedirectUris = { "https://localhost:5002/signout-callback-oidc" },
+               AllowedScopes =
+               {
+                  IdentityServerConstants.StandardScopes.OpenId,
+                  IdentityServerConstants.StandardScopes.Profile,
+                  "api1"
+               }
+            }
+         };
+   }
+}
--- Source/JediOrder.Web/Startup.cs
+++ Source/JediOrder.Web/Startup.cs
@@ -3,12 +3,47 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using IdentityServer4;
+using Microsoft.IdentityModel.Tokens;
 
 namespace JediOrder.Web {
    public class Startup {
       // This method gets called by the runtime. Use this method to add services to the container.
       // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
       public void ConfigureServices(IServiceCollection services) {
+         services.AddControllersWithViews();
+
+         var builder = services.AddIdentityServer()
+            .AddInMemoryIdentityResources(Config.IdentityResources)
+            .AddInMemoryApiScopes(Config.ApiScopes)
+            .AddInMemoryClients(Config.Clients)
+            .AddTestUsers(TestUsers.Users);
+
+         builder.AddDeveloperSigningCredential();
+
+         services.AddAuthentication()
+            .AddGoogle("Google", options =>
+            {
+               options.ClientId = "asdasd";
+               options.ClientSecret = "dsdfsdfsfd";
+            })
+            .AddOpenIdConnect("oidc", "Demo IdentityServer", options =>
+            {
+               options.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
+               options.SignOutScheme = IdentityServerConstants.SignoutScheme;
+               options.SaveTokens = true;
+
+               options.Authority = "https://demo.identityserver.io/";
+               options.ClientId = "interactive.confidential";
+               options.ClientSecret = "secret";
+               options.ResponseType = "code";
+
+               options.TokenValidationParameters = new TokenValidationParameters
+               {
+                  NameClaimType = "name",
+                  RoleClaimType = "role"
+               };
+            });
       }
 
       // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -17,14 +52,18 @@ namespace JediOrder.Web {
             app.UseDeveloperExceptionPage();
          }
 
+         app.UseStaticFiles();
          app.UseRouting();
+
+         app.UseIdentityServer();
+         app.UseAuthorization();
 
          app.UseEndpoints(endpoints =>
          {
             endpoints.MapGet("/", async context =>
                {
-                await context.Response.WriteAsync("Hello World from JediOrder.Web!");
-             });
+                  await context.Response.WriteAsync("Hello World from JediOrder.Web!");
+               });
          });
       }
    }
