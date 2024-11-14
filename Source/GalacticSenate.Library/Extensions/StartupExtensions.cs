3df192e0941e15e11a7d3ef021c17163e904859f
Rick Ryerson <rick.ryerson@outlook.com>
2024-11-13 22:29:08 -0500

    added party service added party events factory

--- Source/GalacticSenate.Library/Extensions/StartupExtensions.cs
+++ Source/GalacticSenate.Library/Extensions/StartupExtensions.cs
@@ -6,6 +6,8 @@ using GalacticSenate.Library.Services.MaritalStatusType;
 using GalacticSenate.Library.Services.MaritalStatusType.Events;
 using GalacticSenate.Library.Services.OrganizationNameValue;
 using GalacticSenate.Library.Services.OrganizationNameValue.Events;
+using GalacticSenate.Library.Services.Party;
+using GalacticSenate.Library.Services.Party.Events;
 using GalacticSenate.Library.Services.PersonNameType;
 using GalacticSenate.Library.Services.PersonNameType.Events;
 using GalacticSenate.Library.Services.PersonNameValue;
@@ -14,33 +16,35 @@ using Microsoft.Extensions.DependencyInjection;
 using System;
 
 namespace GalacticSenate.Library.Extensions {
-   public static class StartupExtensions {
-      public static IServiceCollection AddPeopleAndOrganizations(this IServiceCollection services, EventBusSettings eventBusSettings, EfDataSettings efDataSettings) {
-         if (services is null)
-            throw new ArgumentNullException(nameof(services));
-         if (eventBusSettings is null)
-            throw new ArgumentNullException(nameof(eventBusSettings));
-         if (efDataSettings is null)
-            throw new ArgumentNullException(nameof(efDataSettings));
+    public static class StartupExtensions {
+        public static IServiceCollection AddPeopleAndOrganizations(this IServiceCollection services, EventBusSettings eventBusSettings, EfDataSettings efDataSettings) {
+            if (services is null)
+                throw new ArgumentNullException(nameof(services));
+            if (eventBusSettings is null)
+                throw new ArgumentNullException(nameof(eventBusSettings));
+            if (efDataSettings is null)
+                throw new ArgumentNullException(nameof(efDataSettings));
 
 
-         services.AddEntityFramework(efDataSettings);
+            services.AddEntityFramework(efDataSettings);
 
-         services.AddScoped<IGenderService, GenderService>();
-         services.AddScoped<IMaritalStatusTypeService, MaritalStatusTypeService>();
-         services.AddScoped<IOrganizationNameValueService, OrganizationNameValueService>();
-         services.AddScoped<IPersonNameTypeService, PersonNameTypeService>();
-         services.AddScoped<IPersonNameValueService, PersonNameValueService>();
+            services.AddScoped<IGenderService, GenderService>();
+            services.AddScoped<IMaritalStatusTypeService, MaritalStatusTypeService>();
+            services.AddScoped<IOrganizationNameValueService, OrganizationNameValueService>();
+            services.AddScoped<IPersonNameTypeService, PersonNameTypeService>();
+            services.AddScoped<IPersonNameValueService, PersonNameValueService>();
+            services.AddScoped<IPartyService, PartyService>();
 
-         services.AddEventBus(eventBusSettings);
+            services.AddEventBus(eventBusSettings);
 
-         services.AddSingleton<IGenderEventsFactory, GenderEventsFactory>();
-         services.AddSingleton<IMaritalStatusTypeEventsFactory, MaritalStatusTypeEventsFactory>();
-         services.AddSingleton<IOrganizationNameValueEventsFactory, OrganizationNameValueEventsFactory>();
-         services.AddSingleton<IPersonNameTypeEventsFactory, PersonNameTypeEventsFactory>();
-         services.AddSingleton<IPersonNameValueEventsFactory, PersonNameValueEventsFactory>();
+            services.AddSingleton<IGenderEventsFactory, GenderEventsFactory>();
+            services.AddSingleton<IMaritalStatusTypeEventsFactory, MaritalStatusTypeEventsFactory>();
+            services.AddSingleton<IOrganizationNameValueEventsFactory, OrganizationNameValueEventsFactory>();
+            services.AddSingleton<IPersonNameTypeEventsFactory, PersonNameTypeEventsFactory>();
+            services.AddSingleton<IPersonNameValueEventsFactory, PersonNameValueEventsFactory>();
+            services.AddSingleton<IPartyEventsFactory, PartyEventsFactory>();
 
-         return services;
-      }
-   }
+            return services;
+        }
+    }
 }
