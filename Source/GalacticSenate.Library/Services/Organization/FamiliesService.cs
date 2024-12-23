124daba1fcf7b4430bc1ba0a2e8b8193542c8603
Rick Ryerson <rick.ryerson@outlook.com>
2024-12-23 10:13:18 -0500

    stubbed out family service

--- Source/GalacticSenate.Library/Services/Organization/FamiliesService.cs
+++ Source/GalacticSenate.Library/Services/Organization/FamiliesService.cs
@@ -1,17 +1,38 @@
-﻿using GalacticSenate.Library.Requests;
+﻿using EventBus.Abstractions;
+using GalacticSenate.Data.Implementations.EntityFramework;
+using GalacticSenate.Data.Interfaces;
+using GalacticSenate.Data.Interfaces.Repositories;
+using GalacticSenate.Domain.Model;
+using GalacticSenate.Library.Events;
+using GalacticSenate.Library.Requests;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 using Model = GalacticSenate.Domain.Model;
 
-namespace GalacticSenate.Library.Services.Organization
-{
-    public interface IFamiliesService
-    {
+namespace GalacticSenate.Library.Services.Organization {
+    public interface IFamilyService {
         Task<ModelResponse<Model.Family, AddFamilyRequest>> AddAsync(AddFamilyRequest request);
     }
-    public class FamiliesService
-    {
+    public class FamilyService : InformalOrganizationService, IFamilyService {
+        public FamilyService(IUnitOfWork<DataContext> unitOfWork,
+            IInformalOrganizationRepository informalOrganizationRepository,
+            IOrganizationNameRepository organizationNameRepository,
+            IOrganizationNameValueRepository organizationNameValueRepository,
+            IEventBus eventBus,
+            IEventsFactory eventsFactory,
+            ILogger logger) : base(unitOfWork, 
+                informalOrganizationRepository, 
+                organizationNameRepository, 
+                organizationNameValueRepository, 
+                eventBus, 
+                eventsFactory, logger) {
+        }
+
+        public Task<ModelResponse<Family, AddFamilyRequest>> AddAsync(AddFamilyRequest request) {
+            throw new NotImplementedException();
+        }
     }
 }
