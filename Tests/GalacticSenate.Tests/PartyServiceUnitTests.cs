0c907d94077b0c3502b531d5c812d9e818ba4448
Rick Ryerson <rick.ryerson@outlook.com>
2024-12-24 16:26:51 -0500

    changes

---
+++ Tests/GalacticSenate.Tests/PartyServiceUnitTests.cs
@@ -0,0 +1,36 @@
+﻿using GalacticSenate.Library;
+using GalacticSenate.Library.Requests;
+using GalacticSenate.Library.Services.Party;
+using GalacticSenate.Tests.Properties;
+using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GalacticSenate.Tests {
+    [TestClass]
+    public class PartyServiceUnitTests : PartyServiceFixture {
+        private readonly PartyService partyService;
+
+        public PartyServiceUnitTests() : base("DataContext") {
+            partyService = new PartyService(
+                unitOfWork,
+                partyRepository,
+                eventBusMock.Object,
+                eventsFactory,
+                NullLogger<PartyService>.Instance);
+        }
+
+        [TestMethod]
+        public async Task Add_Test() {
+            var addResponse = await partyService.AddAsync(new AddPartyRequest
+            {
+                Id = Guid.NewGuid(),
+            });
+            Assert.IsTrue(addResponse.Status == StatusEnum.Successful);
+        }
+    }
+}
---
+++ Tests/GalacticSenate.Tests/Properties/PartyServiceFixture.cs
@@ -0,0 +1,20 @@
+﻿using GalacticSenate.Data.Implementations.EntityFramework.Repositories;
+using GalacticSenate.Library.Events;
+using GalacticSenate.Tests.Fixtures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GalacticSenate.Tests.Properties {
+    public abstract class PartyServiceFixture : GalacticSenateFixture {
+        protected readonly PartyRepository partyRepository;
+        protected readonly EventsFactory eventsFactory;
+
+        protected PartyServiceFixture(string databaseName) : base(databaseName) {
+            this.partyRepository = new PartyRepository(unitOfWork);
+            this.eventsFactory = new EventsFactory();
+        }
+    }
+}
